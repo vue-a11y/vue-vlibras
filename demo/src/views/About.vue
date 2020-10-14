1aaa8a51b5eb820ffe742b883224a1a645fafc1f
Alan Ktquez <ktquez@gmail.com>
2020-10-14 19:04:13 -0300

    Add demo

---
+++ demo/src/views/About.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="about">
+    <h1>Esta é a página sobre</h1>
+  </div>
+</template>
---
+++ demo/src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <img
+      alt="Vue logo"
+      src="../assets/logo.png"
+    >
+    <HelloWorld msg="Bem vindo ao seu Vue.js App" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
